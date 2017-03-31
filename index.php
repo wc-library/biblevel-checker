fb1deb28d298d0d9a88e68148c238e62a683416c
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-03-31 17:01:14 -0500

    Converted stub page to PHP and made a header template for usage across application

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = 'Bib Level Checker';
+include_once 'templates/header.php';
+?>
+<body>
+
+<div class="container">
+
+    <div class="header">
+        <h1 class="text-center"><?php echo $title ?></h1>
+    </div>
+
+</div>
+
+</body>
+</html>
--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+/**
+ * Template file for page headers
+ */
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Bib Level Checker</title>
+    <title><?php echo isset($title) ? $title : 'Bib Level Checker' ?></title>
 
     <!-- Stylesheet includes Bootstrap.css + Bootswatch theme -->
     <link rel="stylesheet" href="res/css/index/index.css">
@@ -15,7 +18,4 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </head>
-<body>
 
-</body>
-</html>
