9d3357e6551d5378c9a8683c8eb9d06ccc7363e0
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-04-03 15:16:08 -0500

    Created stub for encodinglevel_handler.php, which will handle the Ajax file uploads.

---
+++ handlers/encodinglevel_handler.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Handles Ajax request for minimum encoding level checking.
+ */
+
+include_once '../config/api_key.php';
+// TODO: Throw exception if $api_key isn't set or is blank
+
--- index.php
+++ index.php
@@ -13,6 +13,7 @@ include_once 'templates/header.php';
     </div>
 
     <div class="well">
+        <?php // TODO: Add text instructing user to upload a list of OCLC numbers (or a text area?) ?>
         <form class="form-vertical" id="file-select-form">
             <div class="input-group form-group">
                 <label class="input-group-btn">
@@ -25,10 +26,13 @@ include_once 'templates/header.php';
                 <input class="form-control form-control-file" type="text" readonly
                        id="file-select-text" placeholder="Select a file." >
             </div>
+            <?php // TODO: Allow user to customize what encoding levels they're looking for ?>
             <input class="btn btn-primary" type="submit"
                    id="file-select-submit" name="file-select-submit" value="Process File">
         </form>
     </div>
+
+    <div id="output"></div>
 
 </div>
 
