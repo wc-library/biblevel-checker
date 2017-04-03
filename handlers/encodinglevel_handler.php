d8995985e46209e208633083e7f33eb359ae66ce
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-04-03 16:43:48 -0500

    More progress on encoding_level.php
    - Renamed get_api_url() to format_api_url() for clarity (it isn't retrieving anything)
    - Created get_bib_resource() which takes an OCLC number and returns the MARCXML data for the resource
    - Wrote stub for check_encoding_level(), which will retrieve the encoding level from the MARCXML string

--- handlers/encodinglevel_handler.php
+++ handlers/encodinglevel_handler.php
@@ -15,7 +15,42 @@ include_once '../config/api_key.php';
  * @param string|int $oclc The OCLC number
  * @return string Formatted WorldCat Search API URL (includes the API key)
  */
-function get_api_url($oclc) {
+function format_api_url($oclc) {
     global $api_key;
     return "http://www.worldcat.org/webservices/catalog/content/$oclc?wskey=$api_key";
 }
+
+
+/**
+ * Retrieves the MARCXML data for a bibliographic resource using curl
+ * @param string|int $oclc The OCLC number
+ * @return mixed MARCXML string of the bibliographic resource
+ */
+function get_bib_resource($oclc) {
+    $url = format_api_url($oclc);
+    // Retrieve MARCXML data for the resource
+    $ch = curl_init();
+    curl_setopt_array($ch, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 30
+    ]);
+    $marcxml_string = curl_exec($ch);
+    curl_close($ch);
+
+    return $marcxml_string;
+}
+
+
+/**
+ * TODO: documentation
+ * @param string $marcxml_string Results of get_bib_resource()
+ */
+function check_encoding_level($marcxml_string) {
+    // TODO: retrieve the encoding level
+}
+
+
+/* Main script */
+
+// TODO: handle file uploads and return encoded data
