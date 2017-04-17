dbe8c3c668681dd39b284783f68f02227510bf51
Connor de la Cruz <connor.c.delacruz@gmail.com>
2017-04-17 16:23:56 -0500

    Preliminary markup for textarea form input

--- index.php
+++ index.php
@@ -13,19 +13,48 @@ include_once 'templates/header.php';
     </div>
 
     <div class="well">
-        <?php // TODO: Add text instructing user to upload a list of OCLC numbers (or a text area?) ?>
+        <?php // TODO: rename to 'encoding-level-form'? ?>
         <form class="form-vertical" id="file-select-form">
-            <div class="input-group form-group">
-                <label class="input-group-btn">
+            <ul class="nav nav-tabs">
+                <li class="active">
+                    <a data-toggle="tab" href="#list-text-pane" id="list-text-tab">
+                        Text
+                    </a>
+                </li>
+                <li>
+                    <a data-toggle="tab" href="#file-select-pane" id="file-select-tab">
+                        Upload File
+                    </a>
+                </li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane fade in active" id="list-text-pane">
+                    <div class="form-group" id="list-text-group">
+                        <label for="list-text-input">
+                            <?php // TODO: phrase more eloquently ?>
+                            Type/paste each OCLC number separated by new lines (i.e. press enter after each number):
+                        </label>
+                        <textarea class="form-control list-text-area" rows="5" required id="list-text-input"
+                                  placeholder="Put each OCLC number on its own line"></textarea>
+                    </div>
+                </div>
+                <?php // TODO: rename 'file-select-*' to 'list-file-*'? ?>
+                <div class="tab-pane fade" id="file-select-pane">
+                    <div class="input-group form-group" id="file-select-group">
+                        <?php // TODO: instructions on what the file should be like ?>
+                        <label class="input-group-btn">
                 <span class="btn btn-default" id="file-select-btn">
                     Choose File
                     <input class="form-control-file" type="file" required
                            id="file-select-input" name="file-select-input" placeholder=""/>
                 </span>
-                </label>
-                <input class="form-control form-control-file" type="text" readonly
-                       id="file-select-text" placeholder="Select a file." >
+                        </label>
+                        <input class="form-control form-control-file" type="text" readonly
+                               id="file-select-text" placeholder="Select a file." >
+                    </div>
+                </div>
             </div>
+
             <div class="form-group">
                 <label class="control-label">
                     Select which encoding levels to highlight:
@@ -88,6 +117,7 @@ include_once 'templates/header.php';
             <input class="btn btn-primary" type="submit" disabled
                    id="file-select-submit" name="file-select-submit" value="Process File">
         </form>
+
     </div>
 
     <div id="output"></div>
